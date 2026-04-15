667bba87c8094fe7e4118ed5dd35bc465c2db554
slouowzee <gael.pilet1@gmail.com>
2026-04-15 12:33:26 +0200

    chore(release): bump to v1.0.0-beta.6

--- internal/updater/updater.go
+++ internal/updater/updater.go
@@ -11,7 +11,7 @@ import (
 )
 
 const (
-	CurrentVersion   = "v1.0.0-beta.5"
+	CurrentVersion   = "v1.0.0-beta.6"
 	githubReleaseURL = "https://api.github.com/repos/slouowzee/kapi/releases/latest"
 )
 
